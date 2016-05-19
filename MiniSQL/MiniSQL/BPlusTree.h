2fbea889029997ccd4ce8ae7522aa13f162b39d7
孙明琦 <crazy95sun@live.cn>
2016-05-19 15:25:15 +0800

    B+tree *2

--- MiniSQL/MiniSQL/BPlusTree.h
+++ MiniSQL/MiniSQL/BPlusTree.h
@@ -13,10 +13,11 @@ public:
         const static size_t key_size = sizeof(key_type);
         using ptr_type = BlockPtr;
         const static size_t ptr_size = sizeof(ptr_type);
-        const static size_t key_count = (BufferBlock::BlockSize - ptr_size - sizeof(size_t) - sizeof(bool)) / (key_size + ptr_size);
+        const static size_t key_count = (BufferBlock::BlockSize - ptr_size - sizeof(size_t) - sizeof(bool) - sizeof(BlockPtr)) / (key_size + ptr_size);
         const static size_t ptr_count = key_count + 1;
         bool is_leaf;
         size_t count;
+        ptr_type parent;
         key_type keys[key_count];
         ptr_type ptrs[ptr_count];
         
@@ -70,15 +71,20 @@ private:
 
     void insert_leaf(BTreeNode* leaf, const TKey& key, const BlockPtr& pValue)
     {
-        leaf->keys[leaf->count] = key;
-        leaf->ptrs[leaf->count] = pValue;
+        auto place = std::find_if(leaf->keys, leaf->keys + leaf->count, [&key](const key_type& ckey) {
+            return ckey > key;
+        });
+        std::move(place, leaf->keys + leaf->count, place + 1);
+        std::move(leaf->ptrs + (place - leaf->keys), leaf->ptrs + leaf->count, leaf->ptrs + (place - leaf->keys) + 1);
+        *place = key;
+        *(leaf->ptrs + (place - leaf->keys)) = pValue;
         leaf->count++;
     }
 
     void insert(const TKey& key, const BlockPtr& pValue)
     {
         BTreeNode* targetLeaf = find_leaf(key);
-        if (targetLeaf->count < BTreeNode::key_count)
+        if (targetLeaf->count < BTreeNode::key_count - 1)
         {
             insert_leaf(targetLeaf, key, pValue);
         }
@@ -89,7 +95,17 @@ private:
             auto newLeaf = &(newBlock.as<BTreeNode>());
             std::copy(targetLeaf->keys, targetLeaf->keys + targetLeaf->count, newLeaf->keys);
             std::copy(targetLeaf->ptrs, targetLeaf->ptrs + targetLeaf->count, newLeaf->ptrs);
-            //
+            insert_leaf(newLeaf, key, pValue);
+            newLeaf->ptrs[BTreeNode::ptr_count - 1] = targetLeaf->ptrs[BTreeNode::ptr_count - 1];
+            targetLeaf->ptrs[BTreeNode::ptr_count - 1] = newBlock;
+            std::fill(targetLeaf->ptrs, targetLeaf->ptrs + targetLeaf->count, nullptr);
+            std::copy(newLeaf->ptrs, newLeaf->ptrs + BTreeNode::key_count / 2, targetLeaf->ptrs);
+            std::copy(newLeaf->keys, newLeaf->keys + BTreeNode::key_count / 2, targetLeaf->keys);
+            targetLeft->count = BTreeNode::key_count / 2;
+            std::move(newLeaf->ptrs + BTreeNode::key_count / 2, newLeaf->ptrs + newLeaf->count, newLeaf->ptrs);
+            std::move(newLeaf->keys + BTreeNode::key_count / 2, newLeaf->keys + newLeaf->count, newLeaf->keys);
+            newLeaf->count = BTreeNode::key_count / 2;
+            
         }
     }
 
